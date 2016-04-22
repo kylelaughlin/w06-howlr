fb0fa775a1e4eb08aec2a19c05e820081098a1a8
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-04-22 15:50:52 -0500

    style new howl; fix links through out app

--- app/controllers/howls_controller.rb
+++ app/controllers/howls_controller.rb
@@ -1,4 +1,20 @@
 get '/howls' do
-  @howls = Howl.all.order('created_at').limit(5)
+  @howls = Howl.all.order(created_at: :DESC).limit(5)
   erb :"howls/index"
+end
+
+get '/howls/new' do
+  @wolves = Wolf.all.order('name')
+  @howl = Howl.new
+  erb :"howls/new"
+end
+
+post '/howls' do
+  @howl = Howl.new(params)
+  @wolves = Wolf.all.order('name')
+  if @howl.save
+    redirect to("/howls")
+  else
+    erb :"howls/new"
+  end
 end
