a3abcb34983e7c3868ae151b983bb4ddd8804060
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-04-23 14:29:19 -0500

    migrate like column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 4) do
+ActiveRecord::Schema.define(version: 5) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 4) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "original_howl_id"
+    t.integer  "likes"
   end
 
   create_table "wolves", force: :cascade do |t|
