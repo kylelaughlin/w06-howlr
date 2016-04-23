64a908c92a415e99b41f375b81e11eadecc9d184
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-04-23 12:05:59 -0500

    Add days_since_howl feature to howls index

--- app/models/howl.rb
+++ app/models/howl.rb
@@ -11,4 +11,32 @@ class Howl < ActiveRecord::Base
 
   belongs_to :wolf
 
+  # Finds the number of days between the current date (today) and the day the
+  # => howl was created and returns the appropriate string.
+  #
+  # Returns a string representing the time period from howl to today
+  def days_since_howl
+    days = (Date.today - self.created_at.to_date).to_i
+    case days
+    when 0
+      "Today"
+    when 1
+      "Yesterday"
+    when 2
+      "2 Days Ago"
+    when 3
+      "3 Days Ago"
+    when 4
+      "4 Days Ago"
+    when 5
+      "5 Days Ago"
+    when 6
+      "6 Days Ago"
+    when 7
+      "Over A Week Ago"
+    else
+      "Long Ago"
+    end
+  end
+
 end
--- tests/test_helper.rb
+++ tests/test_helper.rb
@@ -1,4 +1,5 @@
 require "minitest/autorun"
 require "minitest/rg"
 require "sinatra/activerecord"
-require_relative "../app/models/flash_card.rb"
+require_relative "../app/models/howl.rb"
+require_relative "../app/models/wolf.rb"
