467d635ff5ccfa21ef0cfe60757c68434fdb8094
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-04-23 18:00:42 -0500

    Created javascript file

---
+++ public/my_scripts.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
+}
+
+document.getElementById("like-link").onclick = myOtherFunction;
+
+function myOtherFunction() {
+  var target = document.getElementById("p1");
+  var howlId = target.getAttribute("value");
+  var postData = {howl_id: howlId};
+  alert(postData);
+  $.ajax({url: "/howls/like", method: "post", data: postData, success: function(result){
+    $("#likes").html(result);}
+            //$("#like").html(result);
+        });
+}
